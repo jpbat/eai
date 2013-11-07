8b217f4274364efe6732c4e1164213491402d73d
João Ferreira <jpbat@student.dei.uc.pt>
2013-11-07 18:51:35 +0000

    Corrected EmailDispatcher

--- TP2/BusinessModel/ejbModule/system/EmailDispatcher.java
+++ TP2/BusinessModel/ejbModule/system/EmailDispatcher.java
@@ -14,16 +14,17 @@ import javax.mail.internet.MimeMessage;
 public class EmailDispatcher implements Runnable {
 	
 	private Session session;
-	private String username;
 	private Message message;
 	private Thread t;
 	
+	private String username = "imdbcrawler";
+	private String password = "KUctziBCOxkE";
+	
 	private LinkedBlockingQueue<String[]> pool;
 	
-	public EmailDispatcher (final String username, final String password) {
+	public EmailDispatcher () {
 		
-		this.pool = new LinkedBlockingQueue<>();
-		this.username = username;
+		this.pool = new LinkedBlockingQueue<String[]>();
 		
 		Properties props = new Properties();
 		props.put("mail.smtp.host", "smtp.gmail.com");
@@ -56,7 +57,7 @@ public class EmailDispatcher implements Runnable {
 		while(true) {
 			try {
 				data = this.pool.take();
-				System.out.println("Still got " + this.pool.size() + " emails to send!");
+				System.out.println("Fetched! Mails on pool: " + this.pool.size());
 			} catch (InterruptedException e) {
 			}
 			
@@ -64,12 +65,14 @@ public class EmailDispatcher implements Runnable {
 		}
 	}
 	
-	public void add(String... data) {
-		
-		if (data.length != 3) {
+	public void add(String destiny, String subject, String text) {
+
+		if (destiny == null || subject == null || text == null) {
 			System.out.println("Error adding");
 			return;
 		}
+		
+		String[] data =  {destiny, subject, text};
 		
 		this.pool.add(data);
 	}
@@ -83,8 +86,19 @@ public class EmailDispatcher implements Runnable {
 			Transport.send(message);
 				
 		} catch (Exception e) {
+			System.out.println("Failed to send mail to " + destiny);
+			String[] data = {destiny, subject, text};
+			this.pool.add(data);
 			return false;
+			
 		}
+		System.out.println("Successfuly sent mail to " + destiny);
 		return true;
+	}
+	
+	public static void main(String[] args) {
+		EmailDispatcher em = new EmailDispatcher();
+		
+		em.add("jfms7s@gmail.com", "Hello", "This is the test!!!");
 	}
 }
