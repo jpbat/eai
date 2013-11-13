bebd8dbeee7ba154ee2c324dcead4b9aafef2215
João Silva <raziel7s@gmail.com>
2013-11-13 22:06:49 +0000

    Added mail service

---
+++ TP2/BusinessModel/ejbModule/services/MailService.java
@@ -0,0 +1,21 @@
+package services;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+/**
+ * Session Bean implementation class MailService
+ */
+@Singleton
+@Startup
+public class MailService {
+
+    /**
+     * Default constructor. 
+     */
+    public MailService() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
