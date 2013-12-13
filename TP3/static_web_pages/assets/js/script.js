61cf7ac70788a7fb573a1c8439bc95d2df0076ef
João Ferreira <jpbat@student.dei.uc.pt>
2013-12-13 22:23:17 +0000

    fixed html subscription page

--- TP3/static_web_pages/assets/js/script.js
+++ TP3/static_web_pages/assets/js/script.js
@@ -1,13 +1,22 @@
-function submit() {
-	if ($('#email_subscribe').val().length == 0 && $('#email_unsubscribe').val().length == 0) {
-		alert("At least one email must be filled!");
-		return;
+function addSubscription() {
+	if ($('#email_subscribe').val().length == 0) {
+		alert("Email must be filled!");
+		return false;
 	}
 
-	if ($('#email_subscribe').val().length > 0 && $(':radio:checked').length == 0) {
+	if ($(':radio:checked').length == 0) {
 		alert("You must choose type of subscription!");
-		return;
+		return false;
 	}
 
-	$('form').submit();
+	return true;
+}
+
+function removeSubscription() {
+	if ($('#email_unsubscribe').val().length == 0) {
+		alert("Email must be filled!");
+		return false;
+	}
+
+	return true;
 }
