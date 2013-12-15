febe4d182ad36a5b4bbc36346cc5c3a8cea5d21a
João Ferreira <jpbat@student.dei.uc.pt>
2013-12-15 06:06:45 +0000

    final service added

--- TP3/istp3/src/main/java/services/IMovieStats.java
+++ TP3/istp3/src/main/java/services/IMovieStats.java
@@ -1,5 +1,6 @@
 package services;
 
+import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
@@ -7,7 +8,8 @@ import javax.jws.WebService;
 @WebService
 public interface IMovieStats {
 	
-	@WebResult(name="Data")
-	public String Get(@WebParam(name="Something") String Something);
+	@WebMethod
+	@WebResult(name="result")
+	public String Get(@WebParam(name="option") int option);
 	
 }
